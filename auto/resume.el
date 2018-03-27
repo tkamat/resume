33c33a0ad77eb6e9f0a493ed9b6d6fc280f76af0
Tushaar Kamat <platypusdiamond@gmail.com>
2018-03-26 23:24:43 -0400

    First commit

---
+++ auto/resume.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "resume"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in") ("hyperref" "colorlinks=true" "urlcolor=blue")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "microtype"
+    "graphicx"
+    "titling"
+    "mathabx"
+    "titlesec"
+    "geometry"
+    "hyperref"))
+ :latex)
+
