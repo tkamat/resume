4face4bcd2d245fe773e08be59eb127710058f53
platypusdiamond <platypusdiamond@penguin>
2019-09-16 19:45:40 -0400

    Updated resume.el

--- auto/resume.el
+++ auto/resume.el
@@ -2,15 +2,15 @@
  "resume"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "12pt")))
+                     '(("article" "12pt" "noindent")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=1in") ("hyperref" "colorlinks=true" "urlcolor=blue")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+                     '(("geometry" "margin=0.5in") ("hyperref" "colorlinks=true" "urlcolor=blue")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -19,6 +19,7 @@
     "microtype"
     "graphicx"
     "titling"
+    "setspace"
     "mathabx"
     "titlesec"
     "geometry"
