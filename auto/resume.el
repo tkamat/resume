a52e2fb606db3cc9f81f353ff2ad667b5c1d2573
tushaar <platypusdiamond@gmail.com>
2019-09-16 22:34:08 -0400

    Finished resume and added cover letter

--- auto/resume.el
+++ auto/resume.el
@@ -4,18 +4,19 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt" "noindent")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=0.5in") ("hyperref" "colorlinks=true" "urlcolor=blue")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("fontenc" "T1") ("geometry" "margin=0.5in") ("hyperref" "colorlinks=true" "urlcolor=blue")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art12"
+    "fontenc"
     "microtype"
     "graphicx"
     "titling"
